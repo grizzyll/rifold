59839420809a74ec99dd38a64e45f1708e3079f8
zaki <jakiathallah@gmail.com>
2025-11-06 16:44:48 +0700

    tes

--- resources/views/pages/home/bestseller.blade.php
+++ resources/views/pages/home/bestseller.blade.php
@@ -3,11 +3,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.tailwindcss.com"></script>
-        <title>Rifold - Best Seller</title>
+        <title>Rifold</title>
     </head>
     <body class="bg-[#FBF7F4] text-[#333333]">
 
         @include('components.navbar')
-        
+
     </body>
 </html>
